67a43e457d2c6a70408bb512a15fe9680d28fb21
Noa Verner <noa.verner@post.runi.ac.il>
2025-11-20 22:30:35 +0200

    EXE3

--- Algebra.java
+++ Algebra.java
@@ -5,63 +5,120 @@
 
 public class Algebra {
 	public static void main(String args[]) {
-	    // Tests some of the operations
-	    System.out.println(plus(2,3));   // 2 + 3
-	    System.out.println(minus(7,2));  // 7 - 2
-   		System.out.println(minus(2,7));  // 2 - 7
- 		System.out.println(times(3,4));  // 3 * 4
-   		System.out.println(plus(2,times(4,2)));  // 2 + 4 * 2
-   		System.out.println(pow(5,3));      // 5^3
-   		System.out.println(pow(3,5));      // 3^5
-   		System.out.println(div(12,3));   // 12 / 3    
-   		System.out.println(div(5,5));    // 5 / 5  
-   		System.out.println(div(25,7));   // 25 / 7
-   		System.out.println(mod(25,7));   // 25 % 7
-   		System.out.println(mod(120,6));  // 120 % 6    
-   		System.out.println(sqrt(36));
+		// Tests some of the operations
+		System.out.println(plus(2, 3)); // 2 + 3
+		System.out.println(minus(7, 2)); // 7 - 2
+		System.out.println(minus(2, 7)); // 2 - 7
+		System.out.println(times(3, 4)); // 3 * 4
+		System.out.println(plus(2, times(4, 2))); // 2 + 4 * 2
+		System.out.println(pow(5, 3)); // 5^3
+		System.out.println(pow(3, 5)); // 3^5
+		System.out.println(div(12, 3)); // 12 / 3
+		System.out.println(div(5, 5)); // 5 / 5
+		System.out.println(div(25, 7)); // 25 / 7
+		System.out.println(mod(25, 7)); // 25 % 7
+		System.out.println(mod(120, 6)); // 120 % 6
+		System.out.println(sqrt(36));
 		System.out.println(sqrt(263169));
-   		System.out.println(sqrt(76123));
-	}  
+		System.out.println(sqrt(76123));
+	}
 
 	// Returns x1 + x2
 	public static int plus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int result = x1;
+		if (x2 >= 0) {
+			while (x2 != 0) {
+				result++;
+				x2--;
+			}
+		} else {
+			while (x2 != 0) {
+				result--;
+				x2++;
+			}
+		}
+		return result;
 	}
 
 	// Returns x1 - x2
 	public static int minus(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		return plus(x1, -x2);
 	}
 
 	// Returns x1 * x2
 	public static int times(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		int result = 0;
+		boolean negative = false;
+
+		if (x2 < 0) {
+			x2 = -x2;
+			negative = true;
+		}
+
+		while (x2 != 0) {
+			result = plus(result, x1);
+			x2--;
+		}
+
+		if (negative) {
+			result = -result;
+		}
+		return result;
 	}
 
 	// Returns x^n (for n >= 0)
 	public static int pow(int x, int n) {
-		// Replace the following statement with your code
-		return 0;
+		int result = 1;
+		while (n != 0) {
+			result = times(result, x);
+			n--;
+		}
+		return result;
 	}
 
-	// Returns the integer part of x1 / x2 
+	// Returns the integer part of x1 / x2
 	public static int div(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
+		if (x2 == 0)
+			throw new ArithmeticException("Division by zero");
+		int quotient = 0;
+		boolean negative = false;
+
+		if (x1 < 0) {
+			x1 = -x1;
+			negative = !negative;
+		}
+		if (x2 < 0) {
+			x2 = -x2;
+			negative = !negative;
+		}
+
+		while (x1 >= x2) {
+			x1 = minus(x1, x2);
+			quotient++;
+		}
+
+		if (negative)
+			quotient = -quotient;
+		return quotient;
 	}
 
 	// Returns x1 % x2
 	public static int mod(int x1, int x2) {
-		// Replace the following statement with your code
-		return 0;
-	}	
+		if (x2 == 0)
+			throw new ArithmeticException("Modulo by zero");
+		int quotient = div(x1, x2);
+		int product = times(quotient, x2);
+		return minus(x1, product);
+	}
 
-	// Returns the integer part of sqrt(x) 
+	// Returns the integer part of sqrt(x)
 	public static int sqrt(int x) {
-		// Replace the following statement with your code
-		return 0;
-	}	  	  
+		if (x < 0)
+			throw new ArithmeticException("Square root of negative number");
+		int result = 0;
+		while (times(result, result) <= x) {
+			result++;
+		}
+		return --result; // step back once
+	}
 }
--- Anagram.java
+++ Anagram.java
@@ -28,22 +28,57 @@ public class Anagram {
 
 	// Returns true if the two given strings are anagrams, false otherwise.
 	public static boolean isAnagram(String str1, String str2) {
-		// Replace the following statement with your code
-		return false;
+		String s1 = preProcess(str1);
+		String s2 = preProcess(str2);
+
+		if (s1.length() != s2.length()) {
+			return false;
+		}
+
+		boolean[] used = new boolean[s2.length()];
+		for (int i = 0; i < s1.length(); i++) {
+			char current = s1.charAt(i);
+			boolean found = false;
+			for (int j = 0; j < s2.length(); j++) {
+				if (!used[j] && s2.charAt(j) == current) {
+					used[j] = true;
+					found = true;
+					break;
+				}
+			}
+			if (!found) {
+				return false;
+			}
+		}
+		return true;
 	}
 	   
 	// Returns a preprocessed version of the given string: all the letter characters are converted
 	// to lower-case, and all the other characters are deleted, except for spaces, which are left
 	// as is. For example, the string "What? No way!" becomes "whatnoway"
 	public static String preProcess(String str) {
-		// Replace the following statement with your code
-		return "";
+		StringBuilder builder = new StringBuilder();
+		for (int i = 0; i < str.length(); i++) {
+			char current = str.charAt(i);
+			if (Character.isLetter(current)) {
+				builder.append(Character.toLowerCase(current));
+			}
+		}
+		return builder.toString();
 	} 
 	   
 	// Returns a random anagram of the given string. The random anagram consists of the same
 	// characters as the given string, re-arranged in a random order. 
 	public static String randomAnagram(String str) {
-		// Replace the following statement with your code
-		return "";
+		StringBuilder remaining = new StringBuilder(str);
+		StringBuilder anagram = new StringBuilder();
+
+		while (remaining.length() > 0) {
+			int index = (int) (Math.random() * remaining.length());
+			anagram.append(remaining.charAt(index));
+			remaining.deleteCharAt(index);
+		}
+
+		return anagram.toString();
 	}
 }
